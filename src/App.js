50214204fc8dc44c83480812d64bf16bf34d5174
Ranita Saha <saharanitaa1230@gmail.com>
2022-05-27 02:43:25 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import './App.css';
+// import { Navbar } from "./components";
+// import { Home } from "./pages";
+
+function App() {
+  return (
+    <div className="App">
+    <h1>llll</h1>
+    {/* <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
