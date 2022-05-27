4760d4735b97c83a0723224cd39e77e691bfac1d
Ranita Saha <saharanitaa1230@gmail.com>
2022-05-27 23:07:26 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ export {Home} from "./Home/Home";
 export {Bookmark} from "./Bookmark/Bookmark";
 export {Explore} from "./Explore/Explore";
 export {Profile} from "./Profile/Profile";
-export {Login} from "./Login/Login";
-export {SignUp} from "./SignUp/SignUp";
+export { Login } from "./Auth/Login";
+export { SignUp } from "./Auth/SignUp";
