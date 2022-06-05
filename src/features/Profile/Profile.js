0611efe5b2a948dcb1747a90539e3b5b54a1183e
Ranita Saha <saharanitaa1230@gmail.com>
2022-06-05 18:41:06 +0530

    added notFound page

--- src/features/Profile/Profile.js
+++ src/features/Profile/Profile.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import { LeftSidebar } from "../../components/";
+import React from "react";
+import { LeftSidebar, RightSidebar } from "../../components/";
 
 export const Profile = () => {
   return (
     <main>
-    <div className="container">
-      <LeftSidebar />
-      <div style={{ marginTop: "5rem" }}>Profile</div>
-    </div>
-  </main>
-  
-  )
-}
+      <div className="container">
+        <LeftSidebar />
+        <div className="middle">
+
+          <h3 className="bookmarks">Your Bookmarks</h3>
+          <div className="no-bookmarks">
+
+          </div>
+        </div>
+
+        <RightSidebar />
+      </div>
+    </main>
+  );
+};
