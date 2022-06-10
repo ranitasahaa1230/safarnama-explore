1827ab98795910375ed2a77b22e8edb5c0357627
Ranita Saha <saharanitaa1230@gmail.com>
2022-06-10 13:13:33 +0530

    added react-icons

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,14 @@ import authReducer from "../features/Auth/authSlice";
 import postReducer from "../features/Home/postSlice";
 import allUsersReducer from "../features/Profile/allUsersSlice";
 import profileReducer from "../features/Profile/profileSlice";
+import modalForPostReducer from "../components/MiddleGrid/components/Modal/postModalSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         post: postReducer,
+        // postModal: postModalReducer,
+        modalForPost: modalForPostReducer,
         allUsers: allUsersReducer,
         profile:profileReducer,
     }
